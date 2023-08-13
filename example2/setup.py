750997c77964ea065acc7f9ba502156f7a26ab8d
David Lechner <david@lechnology.com>
2023-08-13 11:16:17 -0500

    workaround

--- example2/setup.py
+++ example2/setup.py
@@ -1,10 +1,23 @@
+import os
+import sysconfig
+
 from setuptools import Extension, setup
+
+INCLUDE_DIRS = (
+    [
+        os.path.join(
+            sysconfig.get_path("data"),
+            "include",
+            "site",
+            f"python{sysconfig.get_python_version()}",
+        )
+    ]
+    if "VIRTUAL_ENV" in os.environ
+    else None
+)
 
 setup(
     ext_modules=[
-        Extension(
-            name="example2.ext",
-            sources=["ext.c"],
-        ),
+        Extension(name="example2.ext", sources=["ext.c"], include_dirs=INCLUDE_DIRS),
     ]
 )
